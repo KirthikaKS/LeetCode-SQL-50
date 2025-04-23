7932b8bfbafa2162102f827158d9b21e74e47777
Kirthika Kulandaivel Senthilkumar <127963062+KirthikaKS@users.noreply.github.com>
2025-04-22 21:33:28 -0500

    [1789] [Time Beats: 95.14%] - LeetPush

--- Primary-Department-for-Each-Employee.sql
+++ Primary-Department-for-Each-Employee.sql
@@ -4,7 +4,7 @@ SELECT employee_id, department_id
 FROM employee
 WHERE primary_flag = 'Y'
 UNION
-SELECT employee_id, department_id
+SELECT employee_id, MIN(department_id) as department_id
 FROM employee
 GROUP BY 1
 HAVING COUNT(*) = 1

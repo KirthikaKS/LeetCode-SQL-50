b6e8b1d03c8dfe139393c61bd221c8a11dfe501b
Kirthika Kulandaivel Senthilkumar <127963062+KirthikaKS@users.noreply.github.com>
2025-06-07 17:14:58 -0500

    [1070] [Time Beats: 90.33%] - LeetPush

--- Product-Sales-Analysis-III.sql
+++ Product-Sales-Analysis-III.sql
@@ -1,10 +1,6 @@
-# Write your MySQL query statement below
-WITH cte AS (SELECT *,
-             DENSE_RANK() OVER(PARTITION BY product_id ORDER BY year) AS rnk
-             FROM sales)
-
+-- Write your PostgreSQL query statement below
 SELECT product_id, year as first_year, quantity, price
-FROM cte
-WHERE rnk = 1
-
-
+FROM sales
+WHERE (product_id, year) IN (SELECT product_id, min(year) 
+                             FROM sales
+                             GROUP BY product_id)

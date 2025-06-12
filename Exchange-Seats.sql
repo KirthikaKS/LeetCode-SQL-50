a4d4d15f02d41d6eebc2e98e3c005a6929eca01f
Kirthika Kulandaivel Senthilkumar <127963062+KirthikaKS@users.noreply.github.com>
2025-06-11 20:53:37 -0500

    [626] [Time Beats: 47.85%] - LeetPush

--- Exchange-Seats.sql
+++ Exchange-Seats.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+/*
 WITH CTE AS (SELECT student,
                 CASE WHEN id % 2 = 1 AND id+1 <= (SELECT MAX(id) FROM Seat) THEN id+1
                 WHEN id % 2 = 0 THEN id-1
@@ -11,3 +12,9 @@ SELECT a.id, b.student
 FROM Seat a
 JOIN CTE b ON a.id = b.new_id
 ORDER BY 1
+*/
+
+SELECT IF(id < (SELECT MAX(id) FROM Seat),(IF(id % 2 = 1, id+1, id-1)),IF(id % 2 = 0, id-1,id)) as id, 
+       student
+FROM Seat
+ORDER BY id

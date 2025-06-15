b65b0a6f2c86fbf37e38d661f889dfcc08cef8bf
Kirthika Kulandaivel Senthilkumar <127963062+KirthikaKS@users.noreply.github.com>
2025-06-15 14:36:58 -0500

    [1327] [Time Beats: 84.82%] - LeetPush

--- List-the-Products-Ordered-in-a-Period.sql
+++ List-the-Products-Ordered-in-a-Period.sql
@@ -1,9 +1,7 @@
--- Write your PostgreSQL query statement below
---- join, group by month year and sum the units
-
-SELECT product_name, SUM(unit) as unit
+# Write your MySQL query statement below
+SELECT p.product_name, SUM(o.unit) as unit
 FROM Products p
 JOIN Orders o ON p.product_id = o.product_id 
-WHERE order_date BETWEEN '2020-02-01' AND '2020-02-29' 
-GROUP BY 1
-HAVING SUM(unit) >= 100
+WHERE o.order_date >= '2020-02-01' AND o.order_date <= '2020-02-29' 
+GROUP BY p.product_name
+HAVING SUM(o.unit) >= 100 ;

ffecbd505021c09959c35cf3b2fe8c788e78d627
Kirthika Kulandaivel Senthilkumar <127963062+KirthikaKS@users.noreply.github.com>
2025-06-15 15:25:53 -0500

    [176] [Time Beats: 68.55%] - LeetPush

--- Second-Highest-Salary.sql
+++ Second-Highest-Salary.sql
@@ -1,8 +1,25 @@
--- Write your PostgreSQL query statement below
-WITH RankedSalaries AS (
+# Write your MySQL query statement below
+/*WITH CTE AS (SELECT *,
+             DENSE_RANK() OVER(ORDER BY salary DESC) AS rnk
+             FROM Employee)
+
+SELECT CASE WHEN c.rnk = 2 THEN c.salary
+            WHEN COUNT(DISTINCT c.id) = 1 THEN null
+       END AS SecondHighestSalary
+FROM CTE c
+WHERE c.rnk = 2 */
+
+
+/*WITH RankedSalaries AS (
   SELECT DISTINCT salary,
          DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
   FROM Employee
 )
 SELECT 
-  (SELECT salary FROM RankedSalaries WHERE rnk = 2) AS SecondHighestSalary;
+  (SELECT salary FROM RankedSalaries WHERE rnk = 2) AS SecondHighestSalary;*/
+
+
+SELECT (SELECT DISTINCT salary
+        FROM Employee
+        ORDER BY salary DESC
+        LIMIT 1 OFFSET 1 ) AS SecondHighestSalary ;

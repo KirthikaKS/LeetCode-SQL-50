14a8fb6441eb630846267e914a9f389055d5d517
Kirthika Kulandaivel Senthilkumar <127963062+KirthikaKS@users.noreply.github.com>
2025-06-15 14:01:19 -0500

    [1484] [Time Beats: 14.41%] - LeetPush

--- Group-Sold-Products-By-The-Date.sql
+++ Group-Sold-Products-By-The-Date.sql
@@ -1,6 +1,8 @@
-# Write your MySQL query statement below
-
-SELECT sell_date, COUNT(DISTINCT product) AS num_sold, GROUP_CONCAT(DISTINCT product ORDER BY product) as products
+-- Write your PostgreSQL query statement below
+SELECT sell_date, 
+       COUNT(DISTINCT product) AS num_sold,
+       STRING_AGG(DISTINCT product, ',' ORDER BY product) as products
 FROM Activities
 GROUP BY 1
 ORDER BY 1
+
